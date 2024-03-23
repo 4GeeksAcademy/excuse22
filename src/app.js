3ae613a06aa09565a3360a7a25c75563fec70c86
2746-sg <156344773+2746-sg@users.noreply.github.com>
2024-03-23 10:44:31 +0000

    proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomIndexFunction = length => {
+    return Math.floor(Math.random() * length);
+  };
+
+  let pronoun = ["the", "Mi our"];
+  let adj = ["grear", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i], adj[a], noun[b]);
+      }
+    }
+  }
 };
