e33d54f3969a5f8eb95808f174d13296b9441847
2746-sg <156344773+2746-sg@users.noreply.github.com>
2024-03-23 11:32:35 +0000

    cambio

--- src/app.js
+++ src/app.js
@@ -6,19 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const randomIndexFunction = length => {
-    return Math.floor(Math.random() * length);
-  };
+  return Math.floor(Math.random() * length);
+};
 
-  let pronoun = ["the", "Mi our"];
-  let adj = ["grear", "big"];
-  let noun = ["jogger", "racoon"];
+let pronoun = ["the", "Mi our"];
+let adj = ["grear", "big"];
+let noun = ["jogger", "racoon"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let a = 0; a < adj.length; a++) {
-      for (let b = 0; b < noun.length; b++) {
-        console.log(pronoun[i], adj[a], noun[b]);
-      }
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < noun.length; b++) {
+      console.log(pronoun[i] + adj[a] + noun[b] + ".com");
     }
   }
-};
+}
